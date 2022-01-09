5ab9daa72e3697fe96e20a101e3f97cb77a33168
AnžeOblak <anze.oblak2@gmail.com>
2022-01-09 15:56:03 +0100

    Light fix

--- code/Light.js
+++ code/Light.js
@@ -1,17 +1,16 @@
 import { Node } from "./Node.js";
 
 export class Light extends Node {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        Object.assign(this, {
-            position         : [10,35,25],
-            ambientColor     : [255,255,255],
-            diffuseColor     : [255,255,255],
-            specularColor    : [255, 255, 255],
-            shininess        : 10,
-            attenuatuion     : [1.0, 0.02, 0]
-        });
-    }
-
+    Object.assign(this, {
+      position: [-15, 40, 0],
+      ambientColor: [200, 200, 200],
+      diffuseColor: [255, 255, 255],
+      specularColor: [100, 100, 100],
+      shininess: 10,
+      attenuatuion: [1.0, 0.015, 0],
+    });
+  }
 }
